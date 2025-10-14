e5cb2fd0267bde2a60930ae10cc99acd0744c99a
Krystian Kluba <klubakrystian@gmail.com>
2025-10-14 16:38:25 +0200

    products-in-cart-review

--- src/js/script.js
+++ src/js/script.js
@@ -271,7 +271,7 @@
         name: thisProduct.data.name,
         amount: thisProduct.amountWidget.value,
         priceSingle: thisProduct.priceSingle,
-        price: thisProduct.priceSingle * thisProduct.amountWidget.value,
+        price: thisProduct.priceSingle,
         params: thisProduct.prepareCartProductParams(),
       };
       console.log(thisProduct);
